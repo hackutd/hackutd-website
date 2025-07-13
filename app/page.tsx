d70b791cedc74e8c0905848e0a99c70dab22e47d
Rayyan Waris <raywa04@gmail.com>
2025-07-12 20:21:58 -0500

    feat: Add init footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen bg-[#070707] text-white overflow-hidden flex justify-center items-center">
-      <div className="w-4/5 mx-auto">
-        <Hero />
-      </div>
-    </main>
+    <div className="min-h-screen bg-[#070707] text-white overflow-hidden">
+      <main className="flex justify-center items-center">
+        <div className="w-4/5 mx-auto">
+          <Hero />
+        </div>
+      </main>
+      <Footer />
+    </div>
   );
 }
