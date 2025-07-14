a5731942b4a0447d7c2a5637f9cc245b77760109
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-07-14 13:45:46 -0500

    tweaked sizes

--- app/components/stats.tsx
+++ app/components/stats.tsx
@@ -8,12 +8,12 @@ export default function Stats() {
         className="
           flex flex-col items-start justify-center
           w-[30vw] h-[35vh]
-          rounded-[1.04rem]
+          rounded-[1rem]
           border border-[rgba(255,255,255,0.35)]
           bg-[rgba(193,193,255,0.20)]
           bg-blend-plus-lighter
           shadow-[-9.2px_-8.6px_39.8px_-9.9px_rgba(0,0,0,0.15),-1.54px_-1.44px_9.94px_-6.63px_rgba(0,0,0,0.10),1.93px_1.80px_14.09px_2.49px_rgba(193,193,255,0.13)_inset,1.16px_1.08px_23.2px_-2.49px_rgba(221,200,255,0.20)_inset]
-          backdrop-blur-[9.26px]
+          backdrop-blur-[9px]
           filter-[drop-shadow(-13.26px_15.74px_47.23px_rgba(96,109,255,0.5))]
           p-10
         "
