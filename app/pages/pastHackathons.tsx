1d57e8e0b336b722c543b87aa29d53756e049e60
Sreevasan Siva <s.sreevasan@gmail.com>
2025-08-04 00:17:15 -0500

    Finished adding links and mobile view

--- app/pages/pastHackathons.tsx
+++ app/pages/pastHackathons.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import React from 'react'
-import HackBadge20 from '@/public/HackBadge20.png'
-import HackBadge21 from '@/public/HackBadge21.png'
-import HackBadge22 from '@/public/HackBadge22.png'
-import HackBadge23 from '@/public/HackBadge23.png'
-import HackBadge24 from '@/public/HackBadge24.png'
+import Hack2020 from '@/public/Hack2020.png'
+import Hack2021 from '@/public/Hack2021.png'
+import Hack2022 from '@/public/Hack2022.png'
+import Hack2023 from '@/public/Hack2023.png'
+import Hack2024 from '@/public/Hack2024.png'
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -18,49 +18,131 @@ const pastHackathons = () => {
         >
           Oh how far we've come...
         </h1>
-        <h3 className = "text-center pt-2">
+        <h3 className = "text-center mb-4">
           Hover over a badge to checkout past hackathons!
         </h3>
-        <div className="w-full max-w-6xl mx-auto flex flex-row mt-2 justify-center items-center flex-wrap py-4">
-             <Link href="https://2024.hackutd.co" target="_blank" rel="noopener noreferrer">
-            <Image
-              src={HackBadge24}
-              alt="Hack Badge 2024"
-              className="w-48 h-48 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
-            />
-             </Link>
-            <Link href="https://2023.hackutd.co" target="_blank" rel="noopener noreferrer">
-            <Image
-              src={HackBadge23}
-              alt="Hack Badge 2023"
-              className="w-48 h-48 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
-            />
-            </Link>
-            <Link href="https://2022.hackutd.co" target="_blank" rel="noopener noreferrer">
-            <Image
-              src={HackBadge22}
-              alt="Hack Badge 2022"
-              className="w-48 h-48 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
-            />
-            </Link>
+        
+        {/* Badges for past hackathons in desktop version */}
+        <div className="hidden lg:flex w-full max-w-7xl mx-auto flex-row items-center px-8 py-4 gap-32 mb-28">
+            
+            <div className = "font-DM-Sans text-center items-center flex flex-col ">
+              <Link href="https://ripple.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2024}
+                alt="Hack Badge 2024"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2024</p>
 
-            <Link href="https://2021.hackutd.co" target="_blank" rel="noopener noreferrer">
-            <Image
-              src={HackBadge21}
-              alt="Hack Badge 2021"
-              className="w-48 h-48 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
-            />
-            </Link>
+            </div>
+            
+            <div className = "font-DM-Sans  text-center items-center flex flex-col "> 
+              <Link href="https://x.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2023}
+                alt="Hack Badge 2023"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2023</p>
+            </div>
+            
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://ix.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2022}
+                alt="Hack Badge 2022"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2022</p>
+            </div>
 
-            <Link href="https://2020.hackutd.co" target="_blank" rel="noopener noreferrer">
-            <Image
-              src={HackBadge20}
-              alt="Hack Badge 2020"
-              className="w-48 h-48 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
-            />
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://prod.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2021}
+                alt="Hack Badge 2021"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2021</p>
+            </div>
+
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://vii.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2020}
+                alt="Hack Badge 2020"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2020</p>
+            </div>
+        </div>
+
+
+        {/* Badges for past hackathons in mobile version */}
+        <div className="flex lg:hidden w-full max-w-7xl mx-auto flex-col md:flex-row items-center px-8 py-4 gap-8 md:gap-32 mb-28">
+          <div className="font-DM-Sans text-center items-center flex flex-col">
+            <Link href="https://ripple.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2024}
+                alt="Hack Badge 2024"
+                className="w-28 h-28 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
             </Link>
+            <p className="mt-4 mb-8 md:mb-0">Hackutd 2024</p>
+          </div>
+
+          <div className = "font-DM-Sans  text-center items-center flex flex-col "> 
+              <Link href="https://x.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2023}
+                alt="Hack Badge 2023"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2023</p>
+            </div>
+            
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://ix.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2022}
+                alt="Hack Badge 2022"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2022</p>
+            </div>
+
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://prod.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2021}
+                alt="Hack Badge 2021"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2021</p>
+            </div>
+
+            <div className = "font-DM-Sans  text-center items-center flex flex-col ">
+              <Link href="https://vii.hackutd.co/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src={Hack2020}
+                alt="Hack Badge 2020"
+                className="w-28 h-28 mx-2 transition-transform duration-200 hover:scale-110 cursor-pointer"
+              />
+              </Link>
+              <p className="mt-4">Hackutd 2020</p>
+            </div>
+          
         </div>
     </div>
+  
   )
 }
 
