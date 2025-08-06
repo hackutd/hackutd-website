bfa3251348ed4f338db7fd96ddab27a34fe882ea
Sreevasan Siva <s.sreevasan@gmail.com>
2025-08-05 20:53:24 -0500

    Embedded links

--- app/pages/openSource.tsx
+++ app/pages/openSource.tsx
@@ -2,18 +2,21 @@
 import hackScreenshot from '@/public/hackScreenshot.png';
 import juryScreenshot from '@/public/juryScreenshot.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function OpenSource() {
   const projects = [
     {
       title: "Hackportal",
       description: "A pre-built portal template made by HackUTD Developers, so you have one less thing to worry about. Used in HackTX, HACKUTA, HackUTD",
-      image: hackScreenshot
+      image: hackScreenshot,
+      url: "https://hackportal.hackutd.co/"
     },
     {
       title: "Jury",
-      description: "A pre-built portal template made by HackUTD Developers, so you have one less thing to worry about. Used in HackTX, HACKUTA, HackUTD",
-      image: juryScreenshot
+      description: "A pre-built judge management tool developed by HackUTD Developers, so running judging is one less thing to worry about. Used at HackUTD, HackTX, and HACKUTA.",
+      image: juryScreenshot,
+      url: "https://jury.mikz.dev/"
     }
   ];
 
@@ -27,12 +30,15 @@ export default function OpenSource() {
           </h2>
           <p className="text-gray-800 text-lg mb-6">
             As active participants in the hackathon community, we’ve built open-source tools to support and empower other organizers.
+            <br/>
+             Click on the projects below to learn more!
           </p>
         </div>
 
         {/* Projects */}
         <div className="flex flex-col lg:flex-row justify-center items-center gap-8 px-4">
           {projects.map((project, index) => (
+            
             <div 
               key={index}
               className="
@@ -44,22 +50,24 @@ export default function OpenSource() {
                 p-4 lg:p-2"
             >
               {/* Project Image */}
-              <div className="
-                w-full
-                h-[140px] 
-                sm:h-[160px] 
-                md:h-[200px] 
-                lg:h-[40vh] lg:w-[30vw] 
-                relative"
-              >
-                <Image
-                  src={project.image}
-                  alt={`${project.title} Screenshot`}
-                  className="rounded-2xl w-full h-full object-cover"
-                  width={800}
-                  height={400}
-                />
-              </div>
+              <Link href={project.url} target="_blank" rel="noopener noreferrer">
+                <div className="
+                  w-full
+                  h-[140px] 
+                  sm:h-[160px] 
+                  md:h-[200px] 
+                  lg:h-[40vh] lg:w-[30vw] 
+                  relative"
+                >
+                  <Image
+                    src={project.image}
+                    alt={`${project.title} Screenshot`}
+                    className="rounded-2xl w-full h-full object-cover"
+                    width={800}
+                    height={400}
+                  />
+                </div>
+              </Link>
 
               {/* Project Description */}
               <div className="p-6 h-auto lg:h-[17vh]">
@@ -71,6 +79,8 @@ export default function OpenSource() {
                 </p>
               </div>
             </div>
+
+            
           ))}
         </div>
       </div>
