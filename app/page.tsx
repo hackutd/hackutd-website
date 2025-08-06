4e683c7a2b258614efaf1be63209c62cec1fbfc6
Sreevasan Siva <s.sreevasan@gmail.com>
2025-08-05 20:36:17 -0500

    Everything merged

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Hero from "./pages/hero";
-import OpenSource from "./pages/openSource";
+import Intro from "./pages/intro";
+import OpenSource from "./pages/opensource";
 import PastHackathons from "./pages/pastHackathons";
 
 export default function Home() {
@@ -8,6 +9,8 @@ export default function Home() {
     <main className="relative min-h-screen bg-[#070707] text-white overflow-hidden flex justify-center items-center">
       <div className="w-4/5 mx-auto">
         <Hero />
+        <div className="my-16" />
+        <Intro />
         <div className="my-16" />
         <OpenSource />
         <div className="my-16" />
