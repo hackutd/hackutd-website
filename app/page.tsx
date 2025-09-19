32a0e9a08e491be463f56a765fd32c61b1218ee6
Shaz Kumar <shaswat_kr@yahoo.com>
2025-09-19 11:35:36 -0500

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-[#070707] text-white overflow-hidden flex justify-center items-center">
-      <div className=" w-full h-[10000px] mx-auto">
-        <Hero />
+      <div className="w-full h-[10000px] mx-auto">
+        <div className="fixed top-0 left-0 right-0 z-50">
+          <Navbar
+            logoSrc="/logo.svg"
+            brandName="HackUTD"
+            links={[
+              { href: "/", label: "Home" },
+              { href: "/about", label: "About" },
+            ]}
+            ctaText="Register"
+            ctaHref="#register"
+          />
+        </div>
+        <div className="pt-16">
+          {" "}
+          {/* Add padding to prevent content from being hidden under the navbar */}
+          <Hero />
+        </div>
       </div>
     </main>
   );
