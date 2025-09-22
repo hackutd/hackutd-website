73df1399ed18ed1042e02fcab6fc090a369343ae
Rayyan Waris <raywa04@gmail.com>
2025-09-22 00:15:22 -0500

    feat: Combine team member comp into intro comp

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import Intro from "./pages/intro";
 import OpenSource from "./pages/openSource";
 import PastHackathons from "./pages/pastHackathons";
 import Footer from "./pages/footer";
-import Members from "./pages/memberstemp";
 
 export default function Home() {
   return (
@@ -23,7 +22,6 @@ export default function Home() {
               links={[
                 { href: "#hero", label: "Home" },
                 { href: "#intro", label: "About" },
-                { href: "#members", label: "Team" },
                 { href: "#past", label: "Hackathons" },
                 { href: "#opensource", label: "Our Projects" },
               ]}
@@ -42,10 +40,6 @@ export default function Home() {
               <Intro />
             </div>
             <div className="my-16" />
-            <div id="members">
-              <Members />
-            </div>
-            <div className="my-16" />
             <div id="past">
               <PastHackathons />
             </div>
@@ -55,23 +49,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* <div className="relative min-h-screen bg-[#070707] text-white overflow-hidden">
-        <main className="flex justify-center items-center">
-          <div className="w-4/5 mx-auto">
-            <Hero />
-            <Stats />
-            <div className="my-16" />
-            <Intro />
-            <div className="my-16" />
-            <Members />
-            <div className="my-16" />
-            <PastHackathons />
-            <div className="my-16" />
-            <OpenSource />
-          </div>
-        </main>
-        <Footer />
-      </div> */}
       </main>
       <Footer />
     </div>
