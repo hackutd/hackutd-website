8413d63afd680d66facd54e03afe7b91f197f3a4
bhuvana <bhuvana.madiraju@gmail.com>
2025-11-03 11:29:32 -0600

    update logos

--- app/components/SponsorCarousel.tsx
+++ app/components/SponsorCarousel.tsx
@@ -7,22 +7,22 @@ export default function SponsorCarousel() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const sponsors = [
-    { name: "Capital One", logo: "/Logos/CapitalOne/CapitalOne Logo.png" },
-    { name: "CBRE", logo: "/Logos/CBRE/CBRE_green.png" },
-    { name: "Cognizant", logo: "/Logos/Cognizant/Cognizant Logo.png" },
-    { name: "EOG", logo: "/Logos/EOG/EOG Logo.png" },
-    { name: "Goldman", logo: "/Logos/Goldman/GS slide 1 (1).png" },
-    { name: "NMC^2", logo: "/Logos/NMC^2/NMC2 Logo from HackUTD.png" },
-    { name: "PNC", logo: "/Logos/PNC/PNC Bank_OrangeGray_650x200 1.png" },
-    { name: "Scale", logo: "/Logos/Scale/scale logo.png" },
-    { name: "State Farm", logo: "/Logos/Statefarm/SF Logo Horizontal.png" },
-    { name: "T-Mobile", logo: "/Logos/T-Mobile/T-Badge_Icon_Ltd-Use_RGB_K_2025-03-06.png" },
-    { name: "Toyota", logo: "/Logos/Toyota/Toyota Logo 2.png" },
-    { name: "Nord_Protect", logo: "/Logos/Nord/Color=Orange, Type=Horizontal, On=Black.png" },
-    { name: "Nord_Incogni", logo: "/Logos/Nord/Incogni_logo_white_better_quality.png" },
-    { name: "Nord_Pass", logo: "/Logos/Nord/NordPass-white-horizontal (2).png" },
-    { name: "Nord_Saily", logo: "/Logos/Nord/saily-logo-white (3).png" },
-    { name: "Nord_VPN", logo: "/Logos/Nord/NordVPN_Logo_RGB_Primary_Blue_White (1).png" },
+    { name: "Capital One", logo: "/Logos/CapitalOne/cap1.png", url: "https://www.capitalone.com/" },
+    { name: "CBRE", logo: "/Logos/CBRE/CBRE_green.png", url: "https://www.cbre.com/" },
+    { name: "Cognizant", logo: "/Logos/Cognizant/CognizantLogo.png", url: "https://www.cognizant.com/us/en" },
+    { name: "EOG", logo: "/Logos/EOG/EOGLogo.png", url: "https://www.eogresources.com/" },
+    { name: "Goldman", logo: "/Logos/Goldman/GS_white.svg", url: "https://www.goldmansachs.com/" },
+    { name: "NMC^2", logo: "/Logos/NMC^2/NMC2 Logo from HackUTD.png", url:"https://nmc2.com/" },
+    { name: "PNC", logo: "/Logos/PNC/PNC Bank_OrangeGray_650x200 1.png", url: "https://www.pnc.com/en/personal-banking.html" },
+    { name: "Scale", logo: "/Logos/Scale/scalelogo.png", url: "https://scale.com/" },
+    { name: "State Farm", logo: "/Logos/Statefarm/SFLogo.png", url: "https://www.statefarm.com/" },
+    { name: "T-Mobile", logo: "/Logos/T-Mobile/T-Badge_Icon_Ltd-Use_RGB_K_2025-03-06.png", url: "https://www.t-mobile.com/" },
+    { name: "Toyota", logo: "/Logos/Toyota/Toyota Logo 2.png", url: "https://www.toyotafinancial.com/us/en.html" },
+    { name: "Nord_Protect", logo: "/Logos/Nord/Color=Orange, Type=Horizontal, On=Black.png", url: "https://nordprotect.com/" },
+    { name: "Nord_Incogni", logo: "/Logos/Nord/Incogni_logo_white_better_quality.png", url: "https://nordvpn.com/incogni/?srsltid=AfmBOooyevCgYKIT5Kr1xutxSdD0_cJV4WnOFErberJRV0g4-9aZroPl" },
+    { name: "Nord_Pass", logo: "/Logos/Nord/NordPass-white-horizontal (2).png", url: "https://nordpass.com/" },
+    { name: "Nord_Saily", logo: "/Logos/Nord/saily-logo-white (3).png", url: "https://saily.com/" },
+    { name: "Nord_VPN", logo: "/Logos/Nord/NordVPN_Logo_RGB_Primary_Blue_White (1).png", url: "https://nordvpn.com/?srsltid=AfmBOopu6SjzTP8a7CtPx1NanVbn9qjjyOS0hFM_tzDs4qTSdNlyFyoF" },
     
 
   ];
@@ -35,7 +35,7 @@ export default function SponsorCarousel() {
 
     let animationFrameId: number;
     let scrollPosition = 0;
-    const scrollSpeed = 0.5; // Adjust speed here (lower = slower)
+    const scrollSpeed = 1.0; // Adjust speed here (lower = slower), og 0.5
 
     const animate = () => {
       scrollPosition += scrollSpeed;
@@ -96,14 +96,21 @@ export default function SponsorCarousel() {
               >
                 <div className="relative w-40 md:w-48 h-24 md:h-32 flex items-center justify-center transition-all duration-300 group-hover:scale-110">
                   <div className="w-full h-full flex items-center justify-center">
-                    <Image
-                      src={sponsor.logo}
-                      alt={sponsor.name}
-                      width={160}
-                      height={80}
-                      className="object-contain opacity-60 group-hover:opacity-100 transition-opacity duration-300 filter grayscale group-hover:grayscale-0"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
+                    <a
+                      href={sponsor.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-block"
+                      >
+                      <Image
+                        src={sponsor.logo}
+                        alt={sponsor.name}
+                        width={160}
+                        height={80}
+                        className="object-contain max-h-16 md:max-h-20 opacity-60 group-hover:opacity-100 transition-opacity duration-300 filter grayscale group-hover:grayscale-0"
+                        style={{ maxWidth: "100%", height: "auto" }}
+                      />
+                    </a>
                   </div>
                 </div>
               </div>
